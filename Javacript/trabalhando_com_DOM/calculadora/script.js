fc850f48e623f70af4f574a8dfb847ebc8cf48d8
Hellen Silva <hellen.aparecidams@outlook.com>
2023-12-02 17:31:48 -0300

    incrementando switch case

--- Javacript/trabalhando_com_DOM/calculadora/script.js
+++ Javacript/trabalhando_com_DOM/calculadora/script.js
@@ -1,4 +1,36 @@
 function Calcular() {
-     let n1 = parseInt (document.getElementById ('num1'))
-    
+
+    let n1 = parseInt(document.getElementById('num1'))
+    let n2 = parseInt(document.getElementById('num2'))
+    let operacao = document.getElementById('operacao')
+    let resultado;
+
+    //Validação para o usuário não digitar palavras e apenas números
+    //A propriedade global NaN é um valor especial que significa Not-A-Number (não é um número).
+    if (NaN(n1) && NoN(n2)) {
+        alert("Somente números")
+    }
+
+    switch (operacao) {
+        case value:
+
+            break;
+
+        case value:
+
+            break;
+
+        case value:
+
+            break;
+            
+        case value:
+
+            break;
+
+        default:
+            break;
+    }
+
 }
+
