aed117726da0f9d11fdd9229df0d63d975633dac
Hellen <hellen.aparecidams@outlook.com>
2023-12-09 15:02:00 -0300

    teste

--- Aula01/trabalhandocomdados/vetores(array).js
+++ Aula01/trabalhandocomdados/vetores(array).js
@@ -8,8 +8,17 @@
 //array em js é heterogenio, ou seja, podemos colocar qualquer coisa, string, int, etc.
 //ainda mais, não possui um tamanho fixo;
 
-//indice = 0 1 2 3 4
+
+//    indice  =   0    1   2    3        4 
 const valores = [18.0, 80, 43, 1.8888, "teste"]
 
-//var (arrays) --> definindo um índice 10 e atribuindo o valor de 967 para o array
-valores[10] = 967 // --> incluindo o índice 10 no array]
+//var(array) -> definindo um indice 10 e atribuindo o valor de 967 para o array
+valores[10] = 967 // --> incluindo o indice 10 no array
+
+console.log(`
+    ${valores[0]}
+    ${valores[3].toFixed(1)} //limita as casas decimais em 2 após a virgula
+    ${valores[10]}
+    ${valores}
+    ${valores.length} //extensão do nosso array (numeral)
+`);
