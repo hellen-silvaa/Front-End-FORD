ed1dff4b589adc01de25472a9808fcdb69bc97f6
Hellen <hellen.aparecidams@outlook.com>
2023-12-09 14:55:26 -0300

    pratica com arrays

---
+++ Aula01/trabalhandocomdados/vetores(array).js
@@ -0,0 +1,15 @@
+//ctrl = shift = n usamos para abrir o code runner
+
+//é como uma caixa com outras caixas dentro, que podem ser acessadas através do seu 
+// índice entre colchetes []. O primeiro índice é sempre o 0 (zero), portanto um vetor de 4 posições terá 
+// índice de 0 a 3. Elas se comportam como variáveis em tudo, e preciso utilizar colchetes tanto na 
+// declaração como no acesso aos dados.
+
+//array em js é heterogenio, ou seja, podemos colocar qualquer coisa, string, int, etc.
+//ainda mais, não possui um tamanho fixo;
+
+//indice = 0 1 2 3 4
+const valores = [18.0, 80, 43, 1.8888, "teste"]
+
+//var (arrays) --> definindo um índice 10 e atribuindo o valor de 967 para o array
+valores[10] = 967 // --> incluindo o índice 10 no array]
