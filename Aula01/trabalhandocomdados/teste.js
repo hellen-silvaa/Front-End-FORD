0d865be33486ce12862e819e4f66caaa6a2cc6ec
Hellen <hellen.aparecidams@outlook.com>
2023-12-14 21:11:27 -0300

    atividade sabado

---
+++ Aula01/trabalhandocomdados/teste.js
@@ -0,0 +1,12 @@
+
+const nomesArray = ["Hellen", "Guilherme", "Ivanilson", "Rafael", "Renata"];
+const sobrenomesArray = ["Silva", "Amorim", "Félix", "Perdigão", "Santos" ];
+
+const nomesCompletosArray = nomesArray.map((element) => {nomesCompletosArray
+    
+});(sobrenomesArray) 
+
+console.log(nomesCompletosArray);
+
+nomesCompletosArray.forEach(nomesCompleto => console.log(nomesCompleto));
+
