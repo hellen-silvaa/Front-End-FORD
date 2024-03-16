138cce812c1de023870632cc63252e2434b58529
Hellen Moura <127620071+hellen-moura@users.noreply.github.com>
2024-03-16 16:40:24 -0300

    Add files via upload

---
+++ AtividadeJS/README.md
@@ -0,0 +1,1 @@
+# condicionaleinterpola-o
---
+++ AtividadeJS/atividadee.js
@@ -0,0 +1,16 @@
+
+const aluno ='Amanda';
+const nota1=5
+const nota2=10
+const nota3=3
+
+const media= (nota1 + nota2 + nota3) / 3
+
+if (media >=7) {
+  console.log(`A média da ${aluno} é ${media.toFixed(1)} está Aprovado`);
+} else if (media <=4.9){
+console.log(`A média da ${aluno} é ${media.toFixed(1)} está Reprovado`);
+} else {(media <= 5 , 6.9)
+console.log(`A média da ${aluno} é ${media.toFixed(1)} está Recuperação`);
+}
+
